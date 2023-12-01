a0fc3ceea95880b49f38f475d0a6b45ce1939c99
SB2224 <sandra@blachier.ca>
2023-11-30 21:09:36 -0500

    fixed JSON code

--- addRecipe_client_side.js
+++ addRecipe_client_side.js
@@ -1,30 +1,34 @@
 var url = "http://localhost:3000/post";
 
 function submitRecipe() {
-    // 
-    var recipeTitle = document.getElementById("recipeTitle").value;
-    var sdescription = document.getElementById("sdescription").value;
-    var mealCategory = document.querySelector('input[name="mealCategory"]:checked').value;
-    var keRequirements = document.getElementById("keRequirements").value;
-    var ingredients = document.getElementById("ingredients").value;
-    var procedure = document.getElementById("procedure").value;
-
-    
-    
-      let pumpkinPie = '{"pumpkin pie" : [' + ' { "recipeTitle":"recipeTitle" , "sdescription":"sdescription","mealCategory": "mealCategory","keRequirements":"keRequirements", "ingredients":"ingredients", "procedures":"procedure" } ] }' ;
+  // 
+  var recipeTitle = document.getElementById("recipeTitle").value;
+  var sdescription = document.getElementById("sdescription").value;
+  var mealCategory = document.querySelector('input[name="mealCategory"]:checked').value;
+  var keRequirements = document.getElementById("keRequirements").value;
+  var ingredients = document.getElementById("ingredients").value;
+  var procedure = document.getElementById("procedure").value;
 
 
-    const recipe = JSON.parse(pumpkinPie);
-    let text = JSON.stringify(recipe);
-    alert(text);
-    localStorage.setItem('recipeTitle sdescription mealCategory keRequirements ingredients procedures JSON', text);
 
-    
-    }
+  //let pumpkinPie = '{"pumpkin pie" : [' + ' { "recipeTitle": recipeTitle , "sdescription": sdescription,"mealCategory": mealCategory , "keRequirements": keRequirements , "ingredients": ingredients , "procedures": procedure } ] }' ;
 
-    function readFromLocalStorage()  {
-        let textsource = localStorage.getItem('recipeTitle sdescription mealCategory keRequirements ingredients procedures JSON');
-        const obj = JSON.parse(textsource);
-        let text = obj.pumpkinPie[0].recipeTitle;
-        alert(text);
-    }
+  let recipes = '{"recipes" : [' + ' { "recipeTitle": "' + recipeTitle + '"' +
+    ' , "sdescription": "' + sdescription + '"' +
+    ' ,"mealCategory": "' + mealCategory + '"' +
+    ' , "keRequirements": "' + keRequirements + '"' +
+    ' , "ingredients": "' + ingredients + '"' +
+    ', "procedures": "' + procedure + '" } ] }';
+
+  alert(recipes);
+  localStorage.setItem('recipes' , recipes);
+
+
+}
+
+function readFromLocalStorage() {
+  let textsource = localStorage.getItem('recipes');
+  const obj = JSON.parse(textsource);
+  let text = obj.recipes[0].recipeTitle;
+  alert(text);
+}
